37aac3aed5ab1dbdfe7b4f0bc66ea23837e79206
Marcos César de Oliveira <mcesarhm@gmail.com>
2019-02-10 11:27:56 -0200

    Add CopyAndDereference function

--- copier.go
+++ copier.go
@@ -1,7 +1,11 @@
 package copier
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"reflect"
+)
 
+// Copy returns a deep copy of src as stored at dst
 func Copy(dst, src interface{}) interface{} {
 	b, err := json.Marshal(src)
 	if err != nil {
@@ -12,4 +16,10 @@ func Copy(dst, src interface{}) interface{} {
 		panic(err)
 	}
 	return dst
+}
+
+// CopyAndDereference returns the deferenced value of the copy of src as stored at dst
+func CopyAndDereference(dst, src interface{}) interface{} {
+	copy := Copy(dst, src)
+	return reflect.ValueOf(copy).Elem()
 }
