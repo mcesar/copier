7fb56c5f26bdeadf616a9cc8060d11f8139c99b7
Marcos César de Oliveira <contato@mcesar.dev>
2019-07-29 16:05:29 -0300

    Add protobuf functions

--- copier.go
+++ copier.go
@@ -1,8 +1,13 @@
 package copier
 
 import (
+	"bytes"
 	"encoding/json"
 	"reflect"
+
+	"github.com/gogo/protobuf/jsonpb"
+
+	"github.com/golang/protobuf/proto"
 )
 
 // Copy returns a deep copy of src as stored at dst
@@ -21,5 +26,25 @@ func Copy(dst, src interface{}) interface{} {
 // CopyAndDereference returns the deferenced value of the copy of src as stored at dst
 func CopyAndDereference(dst, src interface{}) interface{} {
 	copy := Copy(dst, src)
+	return reflect.ValueOf(copy).Elem().Interface()
+}
+
+// CopyPB returns a deep copy of src as stored at dst
+func CopyPB(dst, src proto.Message) interface{} {
+	var buf bytes.Buffer
+	err := (&jsonpb.Marshaler{}).Marshal(&buf, src)
+	if err != nil {
+		panic(err)
+	}
+	err = jsonpb.Unmarshal(&buf, dst)
+	if err != nil {
+		panic(err)
+	}
+	return dst
+}
+
+// CopyPBAndDereference returns the deferenced value of the copy of src as stored at dst
+func CopyPBAndDereference(dst, src proto.Message) interface{} {
+	copy := CopyPB(dst, src)
 	return reflect.ValueOf(copy).Elem().Interface()
 }
